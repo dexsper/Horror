edd769da7d94b90644f8f42dd6a3d8661aa25d0d
YUNGXATIK\young <yungxatik228@mail.ru>
2023-06-07 16:49:42 +0300

    Bot scripts

---
+++ Assets/Scripts/Bots/Bot.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public abstract class Bot : MonoBehaviour
+{
+    [SerializeField] private float _maxRandomWalk;
+
+    protected PlayerModel _playerModel;
+    protected FieldOfView _fieldOfView;
+    protected SurvivorBehaviour _playerBehaviour;
+    protected NavMeshAgent _agent;
+
+    protected Vector3 _targetPosition;
+    public bool IsMove { get; protected set; }
+
+    protected virtual void Awake()
+    {
+        _playerModel = GetComponent<PlayerModel>();
+        _agent = GetComponent<NavMeshAgent>();
+        _fieldOfView = GetComponent<FieldOfView>();
+        _playerBehaviour = GetComponent<SurvivorBehaviour>();
+    }
+
+    private void Update()
+    {
+        Move();
+        UpdateState();
+    }
+
+    protected virtual void UpdateState()
+    {
+        if (_agent.remainingDistance <= _agent.stoppingDistance)
+        {
+            IsMove = false;
+            _targetPosition = Vector3.zero;
+        }
+
+        _playerModel.Animator.SetBool(nameof(IsMove), IsMove);
+    }
+    protected virtual void Move()
+    {
+        var target = GetTargetPosition();
+
+        if(target != Vector3.zero)
+            _targetPosition = target;
+
+        if (_targetPosition == Vector3.zero)
+            _targetPosition = GetRandomPosition();
+
+        if (_targetPosition != Vector3.zero)
+        {
+            _agent.SetDestination(_targetPosition);
+
+            IsMove = true;
+        }
+    }
+
+    protected virtual Vector3 GetTargetPosition()
+    {
+        return Vector3.zero;
+    }
+    protected Vector3 GetRandomPosition()
+    {
+        Vector3 position = Vector3.zero;
+
+        Vector3 direction = Random.insideUnitSphere * Random.Range(_maxRandomWalk / 2f, _maxRandomWalk);
+        direction += transform.position;
+
+        NavMeshHit hit;
+        bool foundPosition = NavMesh.SamplePosition(direction, out hit, _maxRandomWalk, NavMesh.AllAreas);
+
+        if (foundPosition)
+        {
+            NavMeshPath path = new NavMeshPath();
+            _agent.CalculatePath(hit.position, path);
+
+            if (path.status == NavMeshPathStatus.PathComplete)
+                position = hit.position;
+        }
+
+        return position;
+    }
+}
---
+++ Assets/Scripts/Bots/PlayerBot.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerBot : Bot
+{
+    [SerializeField] private List<string> _names;
+    [SerializeField] private TextMeshProUGUI _nameText;
+    
+    private Door _door;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        if(_nameText != null)
+        {
+            _nameText.text = _names[Random.Range(0, _names.Count)];
+        }
+    }
+
+    protected override Vector3 GetTargetPosition()
+    {
+        if (!_playerBehaviour.KeyTaken)
+        {
+            var key = _fieldOfView.GetObject<Key>();
+
+            if (key != null)
+            {
+                return key.transform.position;
+            }
+        }
+
+        if (_door == null)
+        {
+            var door = _fieldOfView.GetObject<Door>();
+
+            if (door != null)
+            {
+                _door = door;
+            }
+        }
+
+        if (_door != null && (_playerBehaviour.KeyTaken || _door.IsOpen))
+        {
+            return _door.transform.position;
+        }
+
+        return Vector3.zero;
+    }
+}
